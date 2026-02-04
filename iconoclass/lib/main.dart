d9963cc57e1ffe7420bcc53fcb8ecd4afbbc798f
ValentinDLC <valentin.dardenne9@gmail.com>
2026-02-04 05:52:11 +0100

    fix: update CardTheme to CardThemeData for Flutter compatibility

--- iconoclass/lib/main.dart
+++ iconoclass/lib/main.dart
@@ -68,7 +68,7 @@ class IconoClassApp extends StatelessWidget {
           ),
 
           // Card theme
-          cardTheme: CardTheme(
+          cardTheme: CardThemeData(
             elevation: 0,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
